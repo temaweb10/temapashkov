70daa28c0c9bbc3abfe83b449dc0db1e86522538
temaweb10 <94466508+temaweb10@users.noreply.github.com>
2022-06-05 19:49:37 +0300

    3

---
+++ script.js
@@ -0,0 +1,10 @@
+function multi(){
+    
+    in1 = document.getElementById('en1').value;
+    in2 = document.getElementById('en2').value;
+    in1 = parseInt ('in1');
+    с = in1 * in2;
+    document.getElementById('res').innerHTML = с;
+    
+}
+
